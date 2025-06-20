d78a6b5e852042557abb520c2cd180dd9a88eb72
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-06-20 21:35:02 +0300

    feat(Employee: update employee)

--- frontend/src/Router/Router.jsx
+++ frontend/src/Router/Router.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Dashboard from '../pages/Dashboard/Dashboard';
+import Employees  from '../pages/Employees/Employees';
+import AddEmployee from '../pages/Employees/AddEmployee';
+import DeletedEmployee from '../pages/Employees/DeletedEmployee';
+import EditEmployee from '../pages/Employees/EditEmployee';
+import EmployeeDetails from '../pages/Employees/ViewEmployee';
 
 const router = createBrowserRouter([
   {
@@ -9,9 +14,15 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { path: '/', element: <Dashboard /> },
+      { path: '/employees', element: <Employees /> },
+      { path: '/add-employee', element: <AddEmployee /> },
+      { path: '/deleted-employees', element: <DeletedEmployee /> },
+      { path: '/edit-employee/:id', element: <EditEmployee /> },
+      { path: '/employee/:id', element: <EmployeeDetails /> },
       { path: '/dashboard', element: <Dashboard /> },
     ]
-  }
+  },
+  
 ]);
 
 export default router; 
