ddaaedbed9b08e3d52e40620167ba73117590a62
Adel-elbamb <engadel987654321@gmail.com>
2025-06-25 17:08:27 +0300

    change the route of apis to services

--- frontend/src/Router/Router.jsx
+++ frontend/src/Router/Router.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Dashboard from '../pages/Dashboard/Dashboard';
-
+import Login from '../pages/Login/Login';
+import ProtectedRoute from './ProtectedRoute';
+import Department from '../pages/Department/ViewDepartments/ViewDepartment';
+import Payroll from '../pages/Payroll/Payroll';
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <MainLayout />,
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    element: <ProtectedRoute />,
     children: [
-      { path: '/', element: <Dashboard /> },
-      { path: '/dashboard', element: <Dashboard /> },
+      {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+          { path: '/', element: <Dashboard /> },
+          { path: '/dashboard', element: <Dashboard /> },
+          { path: '/departments', element: <Department /> },
+          { path: '/payroll', element: <Payroll /> },
+        ]
+      }
     ]
   }
 ]);
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
 import { House, Users, Calendar, FileText, CreditCard, Building2 } from "lucide-react";
 
 const Sidebar = () => {
-  const [active, setActive] = useState("Dashboard");
-
   const menuItems = [
-    { name: "Dashboard", icon: <House size={20} /> },
-    { name: "Employees", icon: <Users size={20} /> },
-    { name: "Attendance", icon: <Calendar size={20} /> },
-    { name: "Leaves", icon: <FileText size={20} /> },
-    { name: "Payroll", icon: <CreditCard size={20} /> },
-    { name: "Departments", icon: <Building2 size={20} /> },
+    { name: "Dashboard", icon: <House size={20} />, path: "/dashboard" },
+    { name: "Employees", icon: <Users size={20} />, path: "/employees" },
+    { name: "Attendance", icon: <Calendar size={20} />, path: "/attendance" },
+    { name: "Leaves", icon: <FileText size={20} />, path: "/leaves" },
+    { name: "Payroll", icon: <CreditCard size={20} />, path: "/payroll" },
+    { name: "Departments", icon: <Building2 size={20} />, path: "/departments" },
   ];
 
   return (
     <div className="sidebar shadow-sm">
       {menuItems.map((item) => (
-        <div
+        <NavLink
           key={item.name}
-          className={`sidebar-item ${active === item.name ? "active" : ""}`}
-          onClick={() => setActive(item.name)}
+          to={item.path}
+          className={({ isActive }) =>
+            `sidebar-item${isActive ? " active" : ""} `
+          }
         >
           <span className="icon">{item.icon}</span>
           <span className="label">{item.name}</span>
-        </div>
+        </NavLink>
       ))}
     </div>
   );
