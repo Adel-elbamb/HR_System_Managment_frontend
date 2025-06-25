4891f43d78b70b7d8e8e4e22939d0e6d1cf29034
ALAAELSAYED20 <alaaelsayedwhab@gmail.com>
2025-06-25 19:29:18 +0300

    Get and Add Attendence

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -5,9 +5,9 @@ import { Outlet } from 'react-router-dom';
 
 export default function MainLayout() {
   return (
-    <div className="container-fluid ps-0 pe-0">
+    <div className="container-fluid ps-0">
       <Header />
-      <div className="row w-100">
+      <div className="row">
         <div className="col-auto">
           <Sidebar />
         </div>
