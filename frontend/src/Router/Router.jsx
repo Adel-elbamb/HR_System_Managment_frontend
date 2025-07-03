665cb8735bd6fe4ce3d680a6ad67bca6ebd8fb3f
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-07-04 00:43:47 +0300

    feat(attendance): update

--- frontend/src/Router/Router.jsx
+++ frontend/src/Router/Router.jsx
@@ -34,6 +34,8 @@ const router = createBrowserRouter([
           { path: '/employee/:id', element: <EmployeeDetails /> },
           { path: '/departments', element: <Department /> },
           { path: '/payroll', element: <Payroll /> },
+          {path:'attendence',element:<Attendence/>},
+          {path:'addattendence',element:<AddAttendence/>},
         ],
       },
     ],
