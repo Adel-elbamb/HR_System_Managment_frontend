57c3cb8cde6b210f50e94ae33a431ce462988b3a
YousefGamgoum <yousefsalah959@gmail.com>
2025-07-13 17:36:15 +0300

    style: update background color, adjust sidebar dimensions, and modify text alignment in Holiday page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Sidebar from '../components/Sidebar/Sidebar';
-import Header from '../components/Header/Header';
-import { Outlet } from 'react-router-dom';
-import HRChatbot from '../pages/HRChatbot/HRChatbot';
+import React from "react";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Header from "../components/Header/Header";
+import { Outlet } from "react-router-dom";
+import HRChatbot from "../pages/HRChatbot/HRChatbot";
 export default function MainLayout() {
   return (
     <div className="container-fluid ps-0 pe-0">
       <Header />
       <div className="row w-100">
-        <div className="col-auto">
+        <div className="col-2">
           <Sidebar />
         </div>
         <div className="col px-0">
           <Outlet />
         </div>
-        <HRChatbot/>
+        <HRChatbot />
       </div>
     </div>
   );
-} 
+}
